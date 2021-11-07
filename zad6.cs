745329ba8f786565dc978e837efff307b88d937f
KornelStanulewicz <47736425+KornelStanulewicz@users.noreply.github.com>
2021-11-07 21:13:51 +0100

    Add files via upload

---
+++ zad6.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad6 : MonoBehaviour
+{
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Obstacle")
+            Debug.Log("collision!");
+    }
+
+}
